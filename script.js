ba92b549ad838e0004d54f44220728ab07c6e451
Gabriel <gvqsilva@gmail.com>
2024-03-24 22:20:37 -0300

    Exercício 2 feito/Gabriel Vasquez/https://github.com/gvqsilva/cp1-dev-web-1sem

--- script.js
+++ script.js
@@ -23,3 +23,24 @@ algum texto e encerrar com ;
 */
 let texto = "Isso é apenas um teste"
 console.log(texto);
+
+
+/*
+Para utilizar os operadores (==, !=, >=) no JS é nescessário 
+utilizar a função if (se), que diz se é true ou false, sendo
+true ele executa a função com os operadores; se for false ele
+não ira executar a função. Quem executará a função é o 
+else (senão)
+*/
+let valor = 10;
+if(valor>7){
+    document.write("Dez é maior que sete!!! <br>");
+}
+if(valor=10){
+    document.write("O valor é igual a dez!!! <br>");
+}
+if(valor != 10){
+    document.write("O valor não é igual a dez!!! <br>");
+}else{
+    document.write("O valor é igual a dez!")
+}
