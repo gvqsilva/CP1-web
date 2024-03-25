cd186e0747fde28ceee6af1f125ce3aa191966b7
Gabriel <gvqsilva@gmail.com>
2024-03-24 21:44:39 -0300

    Exercício 1 feito/Gabriel Vasquez/https://github.com/gvqsilva/cp1-dev-web-1sem

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+Para a variável ter o valor indefinido é 
+nescessário ter a instrução let + algum texto escrito ao lado 
+e um ; no final para encerrar o comando
+*/
+let nome;
+console.log(nome)
+//Utilize a tag console para poder mostrar resultado no inspecionar do google
+
+/*
+Para criar uma variável vazia é nescessário 
+utilizar a função let + algum texto escrito o sinal de =
+ao lado e a variável de null terminando a linha com ;
+*/
+let disciplina = null;
+console.log(disciplina)
+
+/*
+Já para uma variável com valor é nescessário
+seguir os mesmos passos dos outros comando e depois
+do sinal de = abrir as aspas  duplas ("") e escrever
+algum texto e encerrar com ;
+*/
+let texto = "Isso é apenas um teste"
+console.log(texto);
